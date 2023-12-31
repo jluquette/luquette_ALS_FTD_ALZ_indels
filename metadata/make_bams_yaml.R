a46c33005aaef0d46d968bcc12efe7d1137f7b7e
Joe Luquette <lovelace_luquette@hms.harvard.edu>
2023-12-31 14:55:56 -0500

    New script to autogenerate bams.yaml from immutable_metadata.csv. Should just be done internally at some point.

---
+++ metadata/make_bams_yaml.R
@@ -0,0 +1,27 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly=TRUE)
+
+immutable_meta.csv <- args[1]
+out.yaml <- args[2]
+
+if (file.exists(out.yaml)) {
+    stop(paste0('output file ', out.yaml, ' already exists, please delete it first'))
+}
+
+x <- read.table(immutable_meta.csv, header=T, sep=',')
+
+lines <- c()
+for (d in unique(x$donor)) {
+    lines <- c(lines, paste0("'", d, "':"))
+    xd <- x[x$donor == d & x$amp != 'bulk',]
+    lines <- c(lines, paste0("    single_cell:"))
+    for (s in unique(xd$sample))
+        lines <- c(lines, paste0('        ', s, ': bams/', s, '.bam'))
+    xd <- x[x$donor == d & x$amp == 'bulk',]
+    lines <- c(lines, paste0("    bulk:"))
+    for (s in unique(xd$sample))
+        lines <- c(lines, paste0('        ', s, ': bams/', s, '.bam'))
+}
+
+writeLines(text=lines, con=out.yaml)
